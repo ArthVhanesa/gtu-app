cfec880f1496c6c197a063784f35b30e5f7fc263
Kartik Jetani <kartikjetani15@gmail.com>
2022-08-13 19:23:31 +0530

    ± question paper minor changes

--- lib/screens/question_paper_screen.dart
+++ lib/screens/question_paper_screen.dart
@@ -12,6 +12,7 @@ import 'package:gtu_app/components/searchbar.dart';
 import 'package:gtu_app/controllers/papers_controller.dart';
 import 'package:gtu_app/data/card_data.dart';
 import 'package:gtu_app/models/papers_model.dart';
+import 'package:gtu_app/provider/globals.dart';
 import 'package:gtu_app/style/image.dart';
 import 'package:gtu_app/style/style.dart';
 
@@ -28,72 +29,77 @@ class _QuestionPaperScreenState extends State<QuestionPaperScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: AppColors.bgColor,
-      body: SafeArea(
-          child: Column(
-        children: [
-          Header(
-            card: questionPaper,
-          ),
-          Padding(
-            padding: padding,
-            child: SearchBar(
-              searchInputController: questionPaperInputController,
-              onTap: () {
-                questionPaperController
-                    .fetchQuestionPaper(questionPaperInputController.text);
-                FocusManager.instance.primaryFocus?.unfocus();
-              },
-              onSubmitted: (value) {
-                questionPaperController.fetchQuestionPaper(value);
-                FocusManager.instance.primaryFocus?.unfocus();
-              },
-            ),
-          ),
-          const SizedBox(height: 10),
-          Expanded(
-            child: SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              child: Padding(
+    return WillPopScope(
+        onWillPop: () {
+          Get.delete<QuestionPaperController>();
+          return AppGlobals.onBackPressed();
+        },
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: AppColors.bgColor,
+          body: SafeArea(
+              child: Column(
+            children: [
+              Header(
+                card: questionPaper,
+              ),
+              Padding(
                 padding: padding,
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Heading(
-                      heading: 'Previous Question Paper',
-                    ),
-                    questionPaperController.obx(
-                      (questionPaper) {
-                        return ListView.separated(
-                          padding: const EdgeInsets.only(top: 10),
-                          itemCount: questionPaper.length, //length
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          separatorBuilder: (context, index) =>
-                              const SizedBox(height: 10),
-                          itemBuilder: (BuildContext context, int index) {
-                            return QuestionPaperTile(
-                              questionPaper: QuestionPaperModel.fromJson(
-                                  questionPaper[index]),
-                            );
-                          },
-                        );
-                      },
-                      onLoading: CustomLoadingIndicator(),
-                      onError: (error) => NoDataFound(error: error),
-                    ),
-                    PoweredbyAstronApps()
-                  ],
+                child: SearchBar(
+                  searchInputController: questionPaperInputController,
+                  onTap: () {
+                    questionPaperController
+                        .fetchQuestionPaper(questionPaperInputController.text);
+                    FocusManager.instance.primaryFocus?.unfocus();
+                  },
+                  onSubmitted: (value) {
+                    questionPaperController.fetchQuestionPaper(value);
+                    FocusManager.instance.primaryFocus?.unfocus();
+                  },
                 ),
               ),
-            ),
-          )
-        ],
-      )),
-    );
+              const SizedBox(height: 10),
+              Expanded(
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Padding(
+                    padding: padding,
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Heading(
+                          heading: 'Previous Question Paper',
+                        ),
+                        questionPaperController.obx(
+                          (questionPaper) {
+                            return ListView.separated(
+                              padding: const EdgeInsets.only(top: 10),
+                              itemCount: questionPaper.length, //length
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              separatorBuilder: (context, index) =>
+                                  const SizedBox(height: 10),
+                              itemBuilder: (BuildContext context, int index) {
+                                return QuestionPaperTile(
+                                  questionPaper: QuestionPaperModel.fromJson(
+                                      questionPaper[index]),
+                                );
+                              },
+                            );
+                          },
+                          onLoading: CustomLoadingIndicator(),
+                          onError: (error) => NoDataFound(error: error),
+                        ),
+                        PoweredbyAstronApps()
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )),
+        ));
   }
 }
